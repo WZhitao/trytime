824f509051751f0aed96555f0bb5bb4e47743541
WZhitao <154342497+WZhitao@users.noreply.github.com>
2025-07-05 14:45:38 +0800

    Update landslidedatingapp (1).py

--- landslidedatingapp (1).py
+++ landslidedatingapp (1).py
@@ -16,7 +16,7 @@ import ee
 import geemap.foliumap as geemap
 import streamlit as st
 from shapely.geometry import Polygon
-geemap.ee_initialize()
+# geemap.ee_initialize()
 Map = geemap.Map(center=[61.5, 100], zoom=18)
 Map.add_basemap('OpenRailwayMap')
 Map.to_streamlit(height=300)
